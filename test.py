9cbf0f62a1211131d3c3df1c6a9409becd28c4ed
Varun Ved <varunmved@gmail.com>
2016-03-04 18:22:33 -0800

    what is csv

--- test.py
+++ test.py
@@ -1,8 +1,16 @@
 import requests
 import json
+#from pymongo import Connection,bson
 
+def pull():
+    r = requests.get("http://saccounty.cloudapi.junar.com/api/v2/datastreams/CRIME-DATA/data.json/?auth_key=f6fda8ad808a146c09b00118dc0b42f51875238f&limit=100&output=csv")
+    
+    '''
+    with open('crime-data-100.txt', 'w') as outfile:
+        json.dump(r.content, outfile)
+    '''
+    with open('crime-data-100.csv', 'w') as outfile:
+        outfile.write(r.text)
 
-r = requests.get("http://saccounty.cloudapi.junar.com/api/v2/datastreams/SACRA-COUNT-CALIF-QUICK-FACTS/data.json/?auth_key=f6fda8ad808a146c09b00118dc0b42f51875238f")
-with open('data.txt', 'w') as outfile:
-    json.dump(r.content, outfile)
-
+#def mongodbInit():
+pull()
